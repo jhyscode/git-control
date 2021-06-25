0015e4443fb8e8d56b0011f8048b8474d9a2c5ec
e <1770613507@qq.com>
2021-06-25 21:04:54 +0800

    add

---
+++ src/main/java/com/allen/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.allen.controller;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author ：jhys
+ * @date ：Created in 2021/6/25 21:03
+ * @Description ：
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("user controller");
+    }
+}
